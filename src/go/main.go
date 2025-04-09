16a7d3b6f17b1146b846e15ab2d6df30728baec5
argon-dev22 <hyouga.programing@gmail.com>
2025-04-09 21:46:53 +0900

    app: Lambda関数のイベント構造体を追加し、ハンドラーを修正して名前を返すように変更

--- src/go/main.go
+++ src/go/main.go
@@ -7,11 +7,15 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, event string) (string, error) {
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func handler(ctx context.Context, event MyEvent) (string, error) {
 	fmt.Printf("ctx: %v\n", ctx)
 	fmt.Printf("event: %v\n", event)
 
-	return "Hello, Lambda in Go!", nil
+	return fmt.Sprintf("Hello, %s!", event.Name), nil
 }
 
 func main() {

ae98c249a09b38818b24bcf44fa2e8ad9ce4bae5
argon-dev22 <hyouga.programing@gmail.com>
2025-04-09 20:28:11 +0900

    lambda: PythonおよびGoの新しいLambda関数を追加し、ソースコードのパスを更新

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-resource "aws_lambda_function" "this" {
-  function_name = "test-lambda"
+resource "aws_lambda_function" "python" {
+  function_name = "python-function"
   role          = "arn:aws:iam::520196876033:role/s3-event-lambda-dev"
   handler       = "main.lambda_handler"
   runtime       = "python3.10"
-  filename      = "${path.module}/app/lambda_function.zip"
-  source_code_hash = filebase64sha256("${path.module}/app/lambda_function.zip")
+  filename      = "${path.module}/src/python/lambda_function.zip"
+  source_code_hash = filebase64sha256("${path.module}/src/python/lambda_function.zip")
+}
+
+resource "aws_lambda_function" "go" {
+  function_name = "go-function"
+  role          = "arn:aws:iam::520196876033:role/s3-event-lambda-dev"
+  handler       = "main.lambda_handler"
+  runtime       = "go1.x"
+  filename      = "${path.module}/src/go/lambda_function.zip"
+  source_code_hash = filebase64sha256("${path.module}/src/go/lambda_function.zip")
 }
